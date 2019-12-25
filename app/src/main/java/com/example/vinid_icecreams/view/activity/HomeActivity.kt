89514336c7dc7ee4ee36f4cfc3385940fafb65f8
hungpld <hungpldgc1994@gmail.com>
2019-12-25 11:08:26 +0700

    Check location

--- app/src/main/java/com/example/vinid_icecreams/view/activity/HomeActivity.kt
+++ app/src/main/java/com/example/vinid_icecreams/view/activity/HomeActivity.kt
@@ -1,11 +1,9 @@
 package com.example.vinid_icecreams.view.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.example.vinid_icecreams.R
 import com.example.vinid_icecreams.utils.ProgressLoading
 import com.example.vinid_icecreams.view.fragment.chat.FragmentChat
--- app/src/main/java/com/example/vinid_icecreams/view/fragment/store/FragmentStore.kt
+++ app/src/main/java/com/example/vinid_icecreams/view/fragment/store/FragmentStore.kt
@@ -1,9 +1,16 @@
 package com.example.vinid_icecreams.view.fragment.store
 
+import android.content.Context.LOCATION_SERVICE
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vinid_icecreams.R
@@ -12,10 +19,12 @@ import com.example.vinid_icecreams.view.adapter.adapterStore.AdapterStore
 import com.example.vinid_icecreams.view.adapter.adapterStore.OnItemStoreClicklistener
 import kotlinx.android.synthetic.main.fragment_store.*
 
-class FragmentStore : Fragment() ,OnItemStoreClicklistener {
+class FragmentStore : Fragment(),View.OnClickListener ,OnItemStoreClicklistener {
     var mListStore : ArrayList<Store> = ArrayList()
-    var mRcvStore : RecyclerView? = null
+    var mLocationManager : LocationManager? = null
+    var mImgLocation : ImageView? = null
 
+    var mRcvStore : RecyclerView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,7 +38,9 @@ class FragmentStore : Fragment() ,OnItemStoreClicklistener {
 
     private fun initView(view: View?) {
         mRcvStore = view?.findViewById(R.id.rcvStore)
-
+        mImgLocation = view?.findViewById(R.id.imgLocation)
+        mLocationManager = context?.getSystemService(LOCATION_SERVICE) as LocationManager?
+        mImgLocation?.setOnClickListener(this)
     }
 
     private fun setupView() {
@@ -43,5 +54,31 @@ class FragmentStore : Fragment() ,OnItemStoreClicklistener {
 
     override fun onItemClick(positon: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onClick(view: View?) {
+        if(view != null){
+            when(view.id){
+                R.id.imgLocation ->{
+                    try {
+                        // Request location updates
+                        Toast.makeText(context,"Click",Toast.LENGTH_SHORT).show()
+                        mLocationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+                    } catch(ex: SecurityException) {
+                        Log.d("myTag", "Security Exception, no location available")
+                    }
+                }
+            }
+        }
+    }
+
+    //define the listener
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            txtLocation.text = ("" + location.longitude + ":" + location.latitude)
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 }
