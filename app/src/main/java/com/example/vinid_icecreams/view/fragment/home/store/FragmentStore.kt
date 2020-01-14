bb6ada0a4bd2c2c57f9c167eb78668c061ccb19d
hungpld <hungpldgc1994@gmail.com>
2020-01-14 15:17:50 +0700

    fix layout

--- app/src/main/java/com/example/vinid_icecreams/view/fragment/home/store/FragmentStore.kt
+++ app/src/main/java/com/example/vinid_icecreams/view/fragment/home/store/FragmentStore.kt
@@ -12,7 +12,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
