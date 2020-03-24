9c43e82fbeb47927c086e9805acf5ef1caf7c7a8
hungpld <v.hungpld@vinid.net>
2020-03-24 10:02:38 +0700

    fix storeFragment

--- app/src/main/java/com/example/vinid_icecreams/ui/fragment/home/store/StoreFragment.kt
+++ app/src/main/java/com/example/vinid_icecreams/ui/fragment/home/store/StoreFragment.kt
@@ -1,17 +1,12 @@
 package com.example.vinid_icecreams.ui.fragment.home.store
 
-import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context.LOCATION_SERVICE
-import android.content.pm.PackageManager
 import android.location.*
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.vinid_icecreams.R
-import com.example.vinid_icecreams.model.Store
 import com.example.vinid_icecreams.utils.CommonUtils
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -19,6 +14,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.vinid_icecreams.base.fragment.BaseFragment
 import com.example.vinid_icecreams.base.DialogClickListener
 import com.example.vinid_icecreams.di.viewModelModule.ViewModelFactory
+import com.example.vinid_icecreams.model.Store
 import com.example.vinid_icecreams.utils.ProgressLoading
 import com.example.vinid_icecreams.ui.adapter.adapterIndicator.AdapterSliderAd
 import kotlinx.android.synthetic.main.fragment_store.*
@@ -33,6 +29,7 @@ class StoreFragment : BaseFragment<StoreViewModel>(), View.OnClickListener {
     lateinit var viewModelFactory: ViewModelFactory
 
     private var mListStore: ArrayList<Store> = ArrayList()
+
     private var mLocationManager: LocationManager? = null
 
     private val storeViewModel: StoreViewModel by lazy {
@@ -64,17 +61,17 @@ class StoreFragment : BaseFragment<StoreViewModel>(), View.OnClickListener {
 
     override fun setupViewModel() {
         super.setupViewModel()
-        handleGetListStore()
+        getListStore()
         storeViewModel.listStore.observe(viewLifecycleOwner, Observer { data ->
-            ProgressLoading.dismiss()
+            storeController.listStore = data
             mListStore = data
-            storeController.listStore = mListStore
         })
 
         storeViewModel.messageFail.observe(viewLifecycleOwner, Observer {
             showDiaLogFail(ERROR, it, object : DialogClickListener{
                 override fun onConfirmClickListener() {
-                    storeViewModel.getListStore()
+
+                    //storeViewModel.getListStore()
                 }
                 override fun onCancelListener() {
                 }
@@ -85,7 +82,6 @@ class StoreFragment : BaseFragment<StoreViewModel>(), View.OnClickListener {
 
     /*set up list store */
     private fun setupEpoxyRecycleView() {
-        storeController.listStore = mListStore
         rcvStore.run {
             setItemSpacingDp(4)
             setController(storeController)
@@ -96,31 +92,13 @@ class StoreFragment : BaseFragment<StoreViewModel>(), View.OnClickListener {
         if (view != null) {
             when (view.id) {
                 R.id.imgStoreLocation -> {
-                    if (CommonUtils.instace.checkPermission(
-                            requireContext(),
-                            Manifest.permission.ACCESS_FINE_LOCATION
-                        )
-                    ) {
-                        ProgressLoading.show(context)
-                        try {
-                            mLocationManager?.requestLocationUpdates(
-                                LocationManager.NETWORK_PROVIDER,
-                                0L,
-                                0f,
-                                locationListener
-                            )
-                        } catch (ex: SecurityException) {
-                            Log.d("myTag", "Security Exception, no location available")
-                        }
-                    } else {
-                        handleRequestPermission()
-                    }
+
                 }
             }
         }
     }
 
-    private fun handleGetListStore() {
+    private fun getListStore() {
         if (isConnectToNetwork(context)) {
             ProgressLoading.show(context)
             storeViewModel.getListStore()
@@ -133,38 +111,6 @@ class StoreFragment : BaseFragment<StoreViewModel>(), View.OnClickListener {
                 }
             })
         }
-    }
-
-    //handle request permission
-    private fun handleRequestPermission() {
-        val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-        requestPermissions(permissions, CommonUtils.instace.REQUEST_CODE_PEMISSION)
-    }
-
-    /*define the listener*/
-    private val locationListener: LocationListener = object : LocationListener {
-        @SuppressLint("SetTextI18n")
-        override fun onLocationChanged(location: Location) {
-            Log.d("Location", "" + location.longitude + ":" + location.latitude)
-            for (position in 0 until mListStore.size) {
-                val range = CommonUtils.instace
-                    .calculationByDistance(
-                        location.latitude
-                        , location.longitude
-                        , mListStore[position].latitude
-                        , mListStore[position].longitude
-                    )
-                mListStore[position].range = range
-            }
-            mListStore = CommonUtils.instace.sortList(mListStore)
-            storeController.listStore = mListStore
-            storeController.requestModelBuild()
-            ProgressLoading.dismiss()
-        }
-
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-        override fun onProviderEnabled(provider: String) {}
-        override fun onProviderDisabled(provider: String) {}
     }
 
     private fun setLocalName(longitude: Double, latitude: Double) {
@@ -190,30 +136,6 @@ class StoreFragment : BaseFragment<StoreViewModel>(), View.OnClickListener {
             ProgressLoading.dismiss()
         } catch (e: IOException) {
         } catch (e: NullPointerException) {
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            CommonUtils.instace.REQUEST_CODE_PEMISSION -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    try {
-                        ProgressLoading.show(context)
-                        mLocationManager?.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER,
-                            0L,
-                            0f,
-                            locationListener
-                        )
-                    } catch (ex: SecurityException) {
-                        Log.d("myTag", "Security Exception, no location available")
-                    }
-                }
-            }
         }
     }
 
